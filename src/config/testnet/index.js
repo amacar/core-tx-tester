a1a7aabab8c78bff6cc8e0c99a38d71634be3a56
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-06-16 15:53:22 +0200

    Devent and testnet configutation

---
+++ src/config/testnet/index.js
@@ -0,0 +1,4 @@
+const testWallets = require("./wallets")
+const seeds = require("./seeds")
+
+module.exports = { testWallets, seeds }
---
+++ src/config/testnet/wallets.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        "passphrase": "clay harbor enemy utility margin pretty hub comic piece aerobic umbrella acquire",
+        "address": "ANBkoGqWeTSiaEVgVzSKZd3jS7UWzv9PSo",
+        "publicKey": "03287bfebba4c7881a0509717e71b34b63f31e40021c321f89ae04f84be6d6ac37"
+    },
+    {
+        "passphrase": "venue below waste gather spin cruise title still boost mother flash tuna",
+        "address": "AbfQq8iRSf9TFQRzQWo33dHYU7HFMS17Zd",
+        "publicKey": "02def27da9336e7fbf63131b8d7e5c9f45b296235db035f1f4242c507398f0f21d"
+    },
+]
